93f3d0ac0c0e4314258c3a2f9d4782937c5eb0e5
SantosJGND <dourado.jns@gmail.com>
2026-05-18 14:55:43 +0100

    improve data TODOS in student projects

--- bike_sharing/data_injection/config.py
+++ bike_sharing/data_injection/config.py
@@ -20,4 +20,6 @@ CORRUPTION_PRESETS = {
     "outliers": {"type": "outliers", "spike_factor": 3.0, "days": 5, "target_cols": NUMERIC_COLS[:5] + [TARGET_COL]},
     "bias": {"type": "bias", "bias_factor": 0.7, "columns": [TARGET_COL]},
     "schema_drift": {"type": "schema_drift", "old_col": TARGET_COL, "new_col": "bike_count"},
+    "duplicates_light": {"type": "duplicates", "ratio": 0.05},
+    "duplicates_heavy": {"type": "duplicates", "ratio": 0.25},
 }
